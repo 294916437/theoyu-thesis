a28b16e91e063013c7525f749a4038b0d7d24965
Theoyu Du <du2959346375@outlook.com>
2025-12-31 16:26:22 +0800

    chore: revise the config of grpc client

--- sfu/src/utils/grpc-client.ts
+++ sfu/src/utils/grpc-client.ts
@@ -96,12 +96,12 @@ export class GrpcClient {
 				`${config.grpc.host}:${config.grpc.port}`,
 				grpc.credentials.createInsecure(),
 				{
-					"grpc.keepalive_time_ms": 10000,
-					"grpc.keepalive_timeout_ms": 5000,
+					"grpc.keepalive_time_ms": 30000,
+					"grpc.keepalive_timeout_ms": 10000,
 					"grpc.keepalive_permit_without_calls": 1,
 					"grpc.http2.max_pings_without_data": 0,
 					"grpc.http2.min_time_between_pings_ms": 10000,
-					"grpc.http2.min_ping_interval_without_data_ms": 5000,
+					"grpc.http2.min_ping_interval_without_data_ms": 30000,
 				}
 			)
 

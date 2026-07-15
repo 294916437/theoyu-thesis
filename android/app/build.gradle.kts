9c21a6d0ac7233f10188e71a9cc255c97caa65cd
Theoyu Du <du2959346375@outlook.com>
2026-07-15 11:18:50 +0800

    chore: add the libraries config for the network request

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,6 +50,13 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.socketio.client) {
+        exclude(group = "org.json", module = "json")
+    }
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

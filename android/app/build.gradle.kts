f13993d9e1adbcecbe54ae56345a492451a19e01
Theoyu Du <du2959346375@outlook.com>
2026-07-21 15:16:56 +0800

    chore: add the config of the media capability

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -56,6 +56,10 @@ dependencies {
     implementation(libs.okhttp.logging.interceptor)
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
     implementation(libs.socketio.client) {
         exclude(group = "org.json", module = "json")
     }
